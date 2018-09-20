e553844bfdf713c360ec0eca1e983eed21e94be4
camiloriosf-mb <camilo@minutebox.com>
2018-09-20 10:19:30 -0300

    create appBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 // @flow
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Index from './pages/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import Index from "./pages/index";
+import AppBar from "./components/appBar";
 
 // $FlowIgnore - we don't want the missing dom element to be a silent error.
-ReactDOM.render(<Index />, document.querySelector('#root'));
+ReactDOM.render(
+  <AppBar>
+    <Index />
+  </AppBar>,
+  document.querySelector("#root")
+);
